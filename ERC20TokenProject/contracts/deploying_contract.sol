a2a58ccb692201c1d4d70203f6ac7e4e0d2314dd
Parsa <parsa.aminpour@gmail.com>
2023-08-03 12:05:02 +0430

    test commit

--- ERC20TokenProject/contracts/deploying_contract.sol
+++ ERC20TokenProject/contracts/deploying_contract.sol
@@ -2,8 +2,11 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ERC20Token.sol" as TokenContract;
 
-contract exercise is ERC20{
+contract Exercise is ERC20{  // most base like contract
     address public constant OWNER = 0xe2A6c9cFBc1571114ABCF92D5C3C3520434Ee548;
     // bool private paused;
     address[] public users;
@@ -14,6 +17,11 @@ contract exercise is ERC20{
     User public user;
     mapping (address => uint) public users_balances;
     // address[10] public users;
+
+    event adding_contribution_event(
+        address indexed new_cont,
+        uint time
+    );
 
     enum Status { paused, maintane, stopped }
     Status private status;
@@ -51,13 +59,14 @@ contract exercise is ERC20{
         return _status;
     }
 
-    function addContributers(address _addr) public returns(uint conts) {
+    function addContributers(address _addr) public virtual returns(uint conts) {
         // address[] memory users = new address[](10);
         users.push(_addr);
+        emit adding_contribution_event(_addr, block.timestamp);
         return users.length;
     } 
 
-    function efficient_remove(uint _index) public returns(uint) {
+    function efficient_remove(uint _index) public virtual {
         require(_index < users.length, 'the index is overflow');
 
         users[_index] = users[users.length];
@@ -81,4 +90,67 @@ contract exercise is ERC20{
     function test_function(uint _val) public ownerIs returns(bool) {
         return _val == 0 ? true : false;
     }
+}
+
+// contract inheritance should sorted by most base like to most derived
+
+contract dao_for_token is Exercise, Ownable { // most derived contract
+    uint public dest_time;
+    address payable public owner_dao_contract;
+
+    event remove_user_event(address indexed remove_addr, bool remove_status);
+    event received_ether(address indexed sender, uint amount);
+    event send_ether(address indexed to, uint amount, bool status, uint gas);
+
+
+    constructor(uint time, string memory name, string memory symbol, uint total) public 
+    Exercise(name, symbol, total) {
+        dest_time = time;
+        owner_dao_contract = payable(msg.sender);
+    }
+    
+    receive() external payable {
+        emit received_ether(msg.sender, msg.value);
+    }
+    fallback() external payable {
+        emit received_ether(msg.sender, msg.value);
+    }
+    function get_contract_balance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+
+    function addContributers(address _addr) public override(Exercise) returns(uint conts) {
+        require(_addr != address(0), "invalid address");
+        // address[] memory users = new address[](10);
+        users.push(_addr);
+        this.send_ether_to_user(payable(_addr));
+        emit adding_contribution_event(_addr, block.timestamp);
+        return users.length;
+    } 
+
+    function remove_user(address _addr) public onlyOwner returns(bool) {
+        require(_addr != address(0), "invalid address");
+
+        bool removed = false;
+        
+        for(uint i=0; i < users.length; i++) {
+            if(users[i] == _addr) { 
+                Exercise.efficient_remove(i);
+                removed = true;
+            }
+        }
+        
+        emit remove_user_event(_addr, removed);
+        return(removed);
+    }
+
+    function send_ether_to_user(address payable _user) external payable onlyOwner {
+        require(_user != address(0));
+
+        (bool sent, ) = _user.call{value : 10}("");
+        emit send_ether(_user, 10, sent, gasleft());
+        require(sent, "transaction failed");
+    }
+
 }
